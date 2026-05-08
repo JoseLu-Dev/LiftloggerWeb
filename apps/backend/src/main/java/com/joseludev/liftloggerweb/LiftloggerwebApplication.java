e99a489b766ddcdfeaa78f1ea9b56d7443659c28
joseludev <joseludev@gmail.com>
2026-05-09 00:59:31 +0200

    feat(backend): add springdoc-openapi 3 for code-first contract generation

---
+++ apps/backend/src/main/java/com/joseludev/liftloggerweb/LiftloggerwebApplication.java
@@ -0,0 +1,13 @@
+package com.joseludev.liftloggerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftloggerwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftloggerwebApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/com/joseludev/liftloggerweb/LiftloggerwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseludev.liftloggerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftloggerwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
